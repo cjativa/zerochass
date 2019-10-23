c655ca31c7146f9b52da124ccd6e6ad57a89137a
Christopher D Jativa <cjativa@barnesfoundation.org>
2019-10-23 00:42:44 -0400

    functional component

--- .docker-config/node/src/views/about.tsx
+++ .docker-config/node/src/views/about.tsx
@@ -1,23 +1,20 @@
 import * as React from 'react';
 
-export class About extends React.Component {
-
-    render() {
-        return (
-            <div className="about">
-                <div className="content">
-                    <h1>About Zerochass</h1>
-                    <p>Hi there!</p>
-                    <br />
-                    <p>Zerochass is an online learning platform, where you can learn more about software engineering and web development.</p>
-                    <br />
-                    <p>Our goal is to foster a digital community where you can pick up new skills, build scaffolded projects from the ground up, and achieve your software-related dreams, all while keeping track of your progress as you do so.</p>
-                    <br />
-                    <p>We hope you enjoy your time learning along with us and watching what you type come to life.</p>
-                    <br />
-                    <p>-- the Zerochass team</p>
-                </div>
+export const About = () => {
+    return (
+        <div className="about">
+            <div className="content">
+                <h1>About Zerochass</h1>
+                <p>Hi there!</p>
+                <br />
+                <p>Zerochass is an online learning platform, where you can learn more about software engineering and web development.</p>
+                <br />
+                <p>Our goal is to foster a digital community where you can pick up new skills, build scaffolded projects from the ground up, and achieve your software-related dreams, all while keeping track of your progress as you do so.</p>
+                <br />
+                <p>We hope you enjoy your time learning along with us and watching what you type come to life.</p>
+                <br />
+                <p>-- the Zerochass team</p>
             </div>
-        )
-    }
+        </div>
+    )
 }
