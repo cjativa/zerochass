37aea7397c58d1c8f29e711d42cb45fd7a863d07
Christopher D Jativa <cjativa@barnesfoundation.org>
2019-10-23 00:44:46 -0400

    formatting

--- .docker-config/node/src/views/landing/siteBanner.tsx
+++ .docker-config/node/src/views/landing/siteBanner.tsx
@@ -2,18 +2,18 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 
 export const SiteBanner = () => {
-        return (
-            <div className="site-banner">
-                <div className="content">
-                    <div className="heading-container">
-                        <p className="header">learn. code. and innovate</p>
-                        <p className="subheader">practical and goal-oriented resources</p>
-                    </div>
-                    <div className="button-container">
-                        <Link className="browse-btn" to="/">BROWSE TUTORIALS</Link>
-                        {/* <Link className="join-btn" to="/sign-up">JOIN ZEROCHASS</Link> */}
-                    </div>
+    return (
+        <div className="site-banner">
+            <div className="content">
+                <div className="heading-container">
+                    <p className="header">learn. code. and innovate</p>
+                    <p className="subheader">practical and goal-oriented resources</p>
+                </div>
+                <div className="button-container">
+                    <Link className="browse-btn" to="/">BROWSE TUTORIALS</Link>
+                    {/* <Link className="join-btn" to="/sign-up">JOIN ZEROCHASS</Link> */}
                 </div>
             </div>
-        )
+        </div>
+    )
 }
