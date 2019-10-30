199f20999d8146578e7195aa71ebade9e23403f8
Christopher D Jativa <cjativa@barnesfoundation.org>
2019-10-30 19:54:31 -0400

    using prism

--- .docker-config/node/src/views/tutorialComponents/section.tsx
+++ .docker-config/node/src/views/tutorialComponents/section.tsx
@@ -1,6 +1,16 @@
 import * as React from 'react';
 import { Tutorial } from 'interfaces/tutorial';
 import { ProgressCheck } from './progressCheck';
+import marked from 'marked';
+
+import prismjs from 'prismjs';
+import 'prismjs/themes/prism-okaidia.css';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
+import 'prismjs/components/prism-typescript.min.js';
+import 'prismjs/components/prism-bash.min.js';
+import 'prismjs/components/prism-jsx.min.js';
+import 'prismjs/components/prism-tsx.min.js';
 
 interface Props {
     content: Tutorial['tutorialContent'][0];
@@ -16,6 +26,10 @@ export class Section extends React.Component<Props> {
         console.log(`Progress was clicked so marking this`);
     }
 
+    componentDidMount() {
+        prismjs.highlightAll();
+    }
+
     render() {
 
         const { content } = this.props;
@@ -23,11 +37,11 @@ export class Section extends React.Component<Props> {
         const sectionCompleted = null;
 
         return (
-            <section className="section">
+            <section className="section line-numbers">
                 <div className="section-grid">
                     <div className="data">
                         <h2 id={content.sectionTitle}>{content.sectionTitle}</h2>
-                        <p dangerouslySetInnerHTML={{ __html: content.sectionContent }}></p>
+                        <p dangerouslySetInnerHTML={{ __html: marked(content.sectionContent) }}></p>
                         <div className="section-progress">
                             <hr className="section-rule" />
                             {/* <ProgressCheck sectionCompleted={sectionCompleted} onProgressClick={onProgressClick} /> */}
