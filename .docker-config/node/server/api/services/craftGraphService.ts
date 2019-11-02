9f6276ea701e3661e888f04e3ac62f7ab97075b4
Christopher D Jativa <cjativa@barnesfoundation.org>
2019-11-02 11:17:56 -0400

    retrieving tutorial series

--- .docker-config/node/server/api/services/craftGraphService.ts
+++ .docker-config/node/server/api/services/craftGraphService.ts
@@ -6,42 +6,57 @@ import axios from 'axios';
 
 export class CraftGraphService {
 
-    constructor() {
+  constructor() {
+  }
+
+  async getTutorial(slug: string): Promise<TutorialSingle> {
+    const tutorial = (await this.requestHandler('post', tutorialSingleQuery(slug)))[0] as TutorialSingle;
+    return tutorial;
+  }
+
+  async getTutorials(): Promise<TutorialsForContentBanner[]> {
+    const tutorials = await this.requestHandler('post', tutorialContentForBannersQuery()) as TutorialsForContentBanner[];
+    return tutorials;
+  }
+
+  private async requestHandler(method: any, body?: any): Promise<any> {
+    const axiosConfig = new CraftAxiosConfig(method, { query: body });
+
+    try {
+      const payload = (await axios(axiosConfig)).data.data.entries;
+      return payload;
     }
 
-    async getTutorial(slug: string): Promise<TutorialSingle> {
-        const tutorial = (await this.requestHandler('post', tutorialSingleQuery(slug)))[0] as TutorialSingle;
-        return tutorial;
+    catch (error) {
+      console.log(`An error occurred executing a ${method.toUpperCase()} for the query ${body}`, error);
     }
-
-    async getTutorials(): Promise<TutorialsForContentBanner[]> {
-        const tutorials = await this.requestHandler('post', tutorialContentForBannersQuery()) as TutorialsForContentBanner[];
-        return tutorials;
-    }
-
-    private async requestHandler(method: any, body?: any): Promise<any> {
-        const axiosConfig = new CraftAxiosConfig(method, { query: body });
-
-        try {
-            const payload = (await axios(axiosConfig)).data.data.entries;
-            return payload;
-        }
-
-        catch (error) {
-            console.log(`An error occurred executing a ${method.toUpperCase()} for the query ${body}`, error);
-        }
-    }
+  }
 }
 
 
 
 const tutorialContentForBannersQuery = () => {
-    return `
-{
+  return `
+query {
     entries(type: "tutorial") {
     title,
     slug,
      ... on tutorials_tutorial_Entry {
+      color,
+      description {
+        ...on description_description_BlockType {
+          firstLine,
+          secondLine
+        }
+      },
+      featuredImage {
+        url
+      },
+      tags {
+        title
+      }
+    },
+    ... on tutorialSeries_tutorial_Entry {
       color,
       description {
         ...on description_description_BlockType {
@@ -62,11 +77,36 @@ const tutorialContentForBannersQuery = () => {
 }
 
 const tutorialSingleQuery = (slug: string) => {
-    return `
-query {
+  return `
+  query {
     entries(type: "tutorial", slug: "${slug}") {
       title,
        ... on tutorials_tutorial_Entry {
+        color,
+        description {
+            ...on description_description_BlockType {
+              firstLine,
+              secondLine
+            }
+        },
+        featuredImage {
+          url
+        },
+        tags {
+          title
+        },
+        tutorialContent {
+          ...on tutorialContent_sectionBlock_BlockType {
+            sectionTitle,
+            sectionContent,
+            id,
+            uid,
+            dateUpdated, 
+            dateCreated
+          },
+        }
+      },
+    ... on tutorialSeries_tutorial_Entry {
         color,
         description {
             ...on description_description_BlockType {
@@ -94,4 +134,85 @@ query {
     }
   }
 `
+}
+
+const allEntriesQuery = () => {
+  return `
+  query {
+    entries(type: "tutorial", status: "live") {
+      title,
+       ... on tutorials_tutorial_Entry {
+        color,
+        typeHandle,
+        description {
+          ...on description_description_BlockType {
+            firstLine,
+            secondLine
+          }
+        },
+        featuredImage {
+          id,
+          url
+        },
+        tags {
+          uid,
+          id,
+          title
+        },
+        tutorialContent {
+          ...on tutorialContent_sectionBlock_BlockType {
+            sectionTitle,
+            sectionContent,
+            id,
+            uid,
+            dateUpdated, 
+            dateCreated, 
+            sortOrder,
+          },
+        }
+      },
+      ... on tutorialSeries_tutorial_Entry {
+        color,
+        typeHandle,
+        parent {
+          typeHandle,
+          id,
+          title,
+          children {
+            id,
+            title,
+            lft,
+            rgt,
+          }
+        },
+        description {
+          ...on description_description_BlockType {
+            firstLine,
+            secondLine
+          }
+        },
+        featuredImage {
+          id,
+          url
+        },
+        tags {
+          uid,
+          id,
+          title
+        },
+        tutorialContent {
+          ...on tutorialContent_sectionBlock_BlockType {
+            sectionTitle,
+            sectionContent,
+            id,
+            uid,
+            dateUpdated, 
+            dateCreated, 
+            sortOrder,
+          },
+        }
+      }
+    }
+  }
+  `
 }
