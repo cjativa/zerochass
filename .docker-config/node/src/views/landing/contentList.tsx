47fc935da4ddf12b516fc4cb0a0b8689b1dea930
Christopher D Jativa <cjativa@barnesfoundation.org>
2019-11-01 20:27:30 -0400

    sorting into groups of 4 colors as much as possible

--- .docker-config/node/src/views/landing/contentList.tsx
+++ .docker-config/node/src/views/landing/contentList.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { TutorialService } from "services/tutorialService";
 import { Tutorial } from 'interfaces/tutorial';
 import { TutorialCard } from 'views/landing/tutorialCard';
-import { colors } from 'constants/colors';
+import { colorWeights, colors } from 'constants/colors';
 
 export const ContentList = () => {
 
@@ -21,7 +21,38 @@ export const ContentList = () => {
         const colorA = a.color;
         const colorB = b.color;
 
-        return colors[colorA] - colors[colorB];
+        return colorWeights[colorA] - colorWeights[colorB];
+    }
+
+    const generateFourColor = (tutorials: Tutorial[]): Tutorial[] => {
+
+        const sortedTutorials = [];
+
+        // Extract 4 tutorials in this order as much as possible
+        while (checkForFourColors(tutorials)) {
+            colors.forEach((color) => {
+                const index = tutorials.findIndex((tutorial) => { return tutorial.color === color ; });
+                sortedTutorials.push(tutorials[index]);
+                tutorials.splice(index, 1);
+            });
+        }
+
+        // Sort the remaining ones by color
+        tutorials.sort(byColor);
+
+        return [...sortedTutorials, ...tutorials];
+    }
+
+    const checkForFourColors = (tutorials: Tutorial[]) => {
+
+        let black, pink, teal, white;
+
+        black = (tutorials.some((tutorial) => { return tutorial.color === 'black' })) ? true : false;
+        pink = (tutorials.some((tutorial) => { return tutorial.color === 'pink' })) ? true : false;
+        teal = (tutorials.some((tutorial) => { return tutorial.color === 'teal' })) ? true : false;
+        white = (tutorials.some((tutorial) => { return tutorial.color === 'white' })) ? true : false;
+
+        return black && pink && teal && white;
     }
 
 
@@ -34,11 +65,13 @@ export const ContentList = () => {
     }
 
     const { tutorials } = tutorialState;
+    const sorted = generateFourColor(tutorials);
+
 
     return (
 
         <div className="content-list">
-            {tutorials && tutorials.sort(byColor).map((tutorial, index) => {
+            {sorted.map((tutorial, index) => {
                 return (<TutorialCard tutorial={tutorial} key={index} />)
             })}
         </div>
