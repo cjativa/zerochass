b46ced199479692e312562869844655efc30e58f
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-01-27 22:36:17 -0500

    better positioning

--- .docker-config/node/src/views/landing/tutorialCard.tsx
+++ .docker-config/node/src/views/landing/tutorialCard.tsx
@@ -15,10 +15,10 @@ class TutorialCard extends React.Component<Props> {
 		return (
 			<div className={`tutorial-card tutorial-card--${color.toLowerCase()}`}>
 				<a className={`tutorial-card__link`} href={`/tutorial/${tutorial.slug}`}>
-					<div>
-						<h1 className="tutorial-card__header">{tutorial.title}</h1>
-						<p className="tutorial-card__description">{description[0].firstLine}</p>
-						<p className="tutorial-card__description">{description[0].secondLine}</p>
+					<div className="card-inner">
+						<h1 className="card-inner__header">{tutorial.title}</h1>
+						<p className="card-inner__description">{description[0].firstLine}</p>
+						<p className="card-inner__description">{description[0].secondLine}</p>
 					</div>
 					{featuredImage.length > 0 && <div className="card-image">
 						<img className="tutorial-card__image" src={featuredImage[0].url} />
