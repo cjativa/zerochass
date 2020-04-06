7cab3ab92c9d4b0ee862926d5872bc73ab2c5910
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-04-05 22:20:57 -0400

    restructured header styling for responsive design

--- .docker-config/node/src/views/tutorialComponents/header.tsx
+++ .docker-config/node/src/views/tutorialComponents/header.tsx
@@ -23,7 +23,10 @@ export class Header extends React.Component<Props> {
 
         return (
             <header className={`tutorial-header ${color.toLowerCase()}`} style={style}>
-                <div className="tutorial-header__image">{featuredImage.length > 0 && <img src={featuredImage[0].url} />}</div>
+                <div className="tutorial-header__image">
+                    {featuredImage.length > 0 &&
+                        <img className="featured-image" src={featuredImage[0].url} />}
+                </div>
                 <ul className="tutorial-header__tags">
                     {tags && tags.map((tag, index) => {
                         return (<li key={index}>{`${tag.title} |`}</li>)
