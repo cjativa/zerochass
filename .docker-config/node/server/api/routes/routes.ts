044628aac2ea4b4f11ee34e8da23a67804fafdfd
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-04-11 22:08:01 -0400

    added routes for contact and about entries

--- .docker-config/node/server/api/routes/routes.ts
+++ .docker-config/node/server/api/routes/routes.ts
@@ -1,22 +1,26 @@
 import * as express from 'express';
 
 import { AuthenticationController } from '../controllers/authenticationController';
-import { TutorialsController } from '../controllers/tutorialsController';
+import * as TutorialsController from '../controllers/tutorialsController';
+import * as SinglesController from '../controllers/singlesController';
 
 const router = express.Router();
 
 const ac = new AuthenticationController();
-const tc = new TutorialsController();
 
 // Tutorial routes
-router.get('/tutorials', tc.getTutorials);
-router.get('/tutorial/:slug', tc.getTutorial);
+router.get('/tutorials', TutorialsController.getTutorials);
+router.get('/tutorial/:slug', TutorialsController.getTutorial);
 
 // Authentication routes
 router.post('/login', ac.login);
 router.post('/sign-up', ac.signUp);
 router.get('/verify-account', ac.verifyAccount);
 router.post('/sign-out', ac.signOut);
+
+// Singles routes
+router.get('/about', SinglesController.getAbout);
+router.get('/contact', SinglesController.getContact);
 
 export { router };
   
