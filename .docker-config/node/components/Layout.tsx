ab8789fdabdf18b49fe82f211cb17fa5165a0bbe
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-05-07 02:13:04 -0400

    replaced odd quotes

--- .docker-config/node/components/Layout.tsx
+++ .docker-config/node/components/Layout.tsx
@@ -14,8 +14,8 @@ export const Layout = ({ children, pageTitle, ...props }) => {
                     {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'UA-163465719-1');
+                    gtag("js", new Date());
+                    gtag("config", "UA-163465719-1");
                     `}
                 </script>
             </Head>
