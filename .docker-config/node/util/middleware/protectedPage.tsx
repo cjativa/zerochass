d8ac708a4acbc3a2d21735d8a4c6c66f5c458334
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-06-09 02:04:23 -0400

    augment the request with access token and user id

--- .docker-config/node/util/middleware/protectedPage.tsx
+++ .docker-config/node/util/middleware/protectedPage.tsx
@@ -1,9 +1,15 @@
 export default async function protectPageWithAuthentication({ req, res }) {
     const { isAuthenticated } = (await import('../../util/middleware/protectWithAuthentcation'));
-    const { authenticated } = isAuthenticated(req, res);
-
+    const { authenticated, accessToken, userId } = isAuthenticated(req, res);
+    
     if (!authenticated) {
         res.writeHead(302, { Location: '/' });
         res.end();
+    }
+
+    else {
+        req['authenticated'] = true;
+        req['userId'] = userId;
+        req['accessToken'] = accessToken;
     }
 };
