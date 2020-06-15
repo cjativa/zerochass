012b5a923d9911e778136c5a6543f3c65b6a2b16
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-06-15 01:25:11 -0400

    fetching matching tags using database service

--- .docker-config/node/pages/api/tags.ts
+++ .docker-config/node/pages/api/tags.ts
@@ -1,9 +1,9 @@
-import { TagService } from '../../util/services/tagService';
+import { TagDatabaseService } from '../../util/database/classes/tagDatabaseService';
 
 const handler = async (request, response) => {
 
     const { tag } = request.query;
-    const matchingTags = await TagService.findMatchingTag(tag);
+    const matchingTags = await TagDatabaseService.findMatchingTag(tag);
 
     response.json(matchingTags);
 };
