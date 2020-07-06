47f30580c8c46174640bcc7ef9503a8b64e01c1e
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-07-05 23:07:58 -0400

    Planner class is now a default export

--- .docker-config/node/pages/api/sections/[sectionId].ts
+++ .docker-config/node/pages/api/sections/[sectionId].ts
@@ -11,21 +11,22 @@ const handler = async (request, response) => {
     catch {
         console.log(`An error occurred performing a ${method} request on /section`);
     }
-
-
 };
 
+/** Handles updating the progress of the section */
 const updateSectionProgress = async (request, response) => {
 
+    // Get the section id and whether the section should be complete for this user
     const {
         query: { sectionId },
         body: { complete },
         userId
     } = request;
 
+    // If the section should be completed, mark it as complete
     if (complete == true) await TutorialProgressManager.setSectionComplete(userId, sectionId);
 
-
+    // If the section should not be completed, mark it as not complete
     if (complete == false) await TutorialProgressManager.setSectionIncomplete(sectionId);
 
     response.json(`Completed`);
