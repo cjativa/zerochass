c7e1d3bf7a6bdda4455c9c159d9096b5767b8303
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-07-06 23:18:35 -0400

    working tutorial section progress endpoints

--- .docker-config/node/pages/api/sections/[sectionId].ts
+++ .docker-config/node/pages/api/sections/[sectionId].ts
@@ -1,15 +1,18 @@
 import { TutorialProgressManager } from '../../../util/database/classes/tutorialProgressManager';
+import protectWithAuthentication from '../../../util/middleware/protectWithAuthentcation';
+
+
 const handler = async (request, response) => {
 
     const { method } = request;
 
     try {
-        if (method === 'PUT') await updateSectionProgress(request, response);
+        if (method === 'POST') await updateSectionProgress(request, response);
 
     }
 
-    catch {
-        console.log(`An error occurred performing a ${method} request on /section`);
+    catch (error) {
+        console.log(`An error occurred performing a ${method} request on /section`, error);
     }
 };
 
@@ -17,20 +20,22 @@ const handler = async (request, response) => {
 const updateSectionProgress = async (request, response) => {
 
     // Get the section id and whether the section should be complete for this user
-    const {
+    let {
         query: { sectionId },
         body: { complete },
         userId
     } = request;
     let sectionProgress;
 
+    sectionId = parseInt(sectionId);
+
     // If the section should be completed, mark it as complete
     if (complete == true)  sectionProgress = await TutorialProgressManager.setSectionComplete(userId, sectionId);
 
     // If the section should not be completed, mark it as not complete
-    if (complete == false) sectionProgress = await TutorialProgressManager.setSectionIncomplete(sectionId);
+    if (complete == false) sectionProgress = await TutorialProgressManager.setSectionIncomplete(sectionId, userId);
 
     response.json(sectionProgress);
 };
 
-export default handler;
+export default protectWithAuthentication(handler);
