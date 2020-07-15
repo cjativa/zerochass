c3d25f23f04670da6c2abab6fa9c0b31d89b6753
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-07-14 21:30:06 -0400

    displaying the profile picture of the author and name

--- .docker-config/node/components/tutorial/tutorialHeader.tsx
+++ .docker-config/node/components/tutorial/tutorialHeader.tsx
@@ -1,21 +1,30 @@
 export const TutorialHeader = (props) => {
-    const { title, tags, featuredImage, color } = props;
+    const { title, tags, featuredImage, color, authorHeading, authorImage, authorName } = props;
 
     return (
         <header className={`tutorial-header ${color.toLowerCase()}`}>
+
+            {/** Display information about the author */}
+            <div className="tutorial-header__author">
+                <img src={authorImage} className="tutorial-header__author-image" />
+                <span>{authorName}</span>
+            </div>
+
+            {/** Display the tutorial featured image */}
             <div className="tutorial-header__image">
                 {featuredImage.length > 0 &&
                     <img className="featured-image" src={featuredImage} />}
             </div>
+
+            {/** Display the available tags */}
             <ul className="tutorial-header__tags">
                 {tags && tags.map((tag, index) =>
-                    <li className="tutorial-header__tag"
-                        key={index}
-                    >
+                    <li className="tutorial-header__tag" key={index} >
                         {`#${tag.tag}`}
                     </li>
                 )}
             </ul>
+
             <h1 className="tutorial-header__title">{title}</h1>
         </header>
     )
