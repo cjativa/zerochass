f3ec818c8d3f319fb81621da1a4528ece6784355
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-07-15 22:42:19 -0400

    added author and image

--- .docker-config/node/components/tutorials/TutorialCard.tsx
+++ .docker-config/node/components/tutorials/TutorialCard.tsx
@@ -7,7 +7,8 @@ interface TutorialCardProps {
 
 export const TutorialCard = (props: TutorialCardProps) => {
 
-    const { featuredImage, title, color, description2, slug } = props.tutorial;
+    const { featuredImage, title, color, description2, slug,
+        profileImage: authorImage, name: authorName } = props.tutorial;
 
     let titleClass = 'tl-card__title';
     let subtitleClass = 'tl-card__subtitle';
@@ -20,12 +21,21 @@ export const TutorialCard = (props: TutorialCardProps) => {
     return (
         <Link href={`/tutorial/${slug}`}>
             <a className="tl-card" >
+
+                {/** Display featured image */}
                 <div className={`tl-card__top ${color}`}  >
                     <img className="tl-card__image" src={featuredImage} />
                     <div className="tl-card__layer" />
                 </div>
+
+                {/** Display bottom of card */}
                 <div className={`tl-card__bottom`}>
                     <h1 className={titleClass}>{title}</h1>
+                    
+                    <div className="tl-card__author">
+                        <img src={authorImage} className="tl-card__author-image" />
+                        <span>{authorName}</span>
+                    </div>
                     <p className={subtitleClass}>{description2}</p>
                 </div>
             </a>
