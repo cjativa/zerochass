6e95a37625360973d57b76b5dac6c4d54b0de986
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-07-17 23:53:09 -0400

    moved planner to own component and added styling

---
+++ .docker-config/node/components/planner/planner.tsx
@@ -0,0 +1,20 @@
+import { TutorialCard } from '../tutorials/TutorialCard';
+
+export const Planner = ({ tutorials }) => {
+    return (
+        <div className="planner padded-page">
+            <div className="padded-page__content">
+                <h1>Planner</h1>
+                <p>Here's where you'll find tutorials you've started</p>
+
+                {/** Display the cards of enrolled tutorials */}
+                <div className="planner__tutorials">
+                    {tutorials.map((tutorial) =>
+                        <TutorialCard tutorial={tutorial} />
+                    )}
+                </div>
+
+            </div>
+        </div>
+    )
+};
