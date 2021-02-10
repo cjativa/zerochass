bb83ee340861cac522aeca60c14332f779e1d129
Christopher D Jativa <cjativa@barnesfoundation.org>
2021-02-10 01:19:44 -0500

    no longer need these routes

--- .docker-config/node/server/api/routes/plannerRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import PlannerController from '../controllers/plannerController';
-
-// Create the Planner Router
-const PlannerRouter = express.Router();
-
-PlannerRouter.get('/enroll', PlannerController.handleTutorialEnrollment);
-
-export default PlannerRouter;
--- .docker-config/node/server/api/routes/tutorialRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import TutorialController from '../controllers/tutorialController';
-
-// Create the Tutorial Router
-const TutorialRouter = express.Router();
-
-TutorialRouter.get('/:slug', TutorialController.getTutorial);
-
-export default TutorialRouter;
