0d928c526dd177d816f843621cf57c883659722e
Christopher D Jativa <cjativa@barnesfoundation.org>
2021-02-11 00:51:29 -0500

    combined knex configuration

--- .docker-config/node/server/database/knex.ts
+++ .docker-config/node/server/database/knex.ts
@@ -1,5 +1,39 @@
 import knex from 'knex';
-import { KnexConfiguration } from '../knexfile';
 import Config from '../utils/config';
+
+const KnexConfiguration = {
+    development: {
+        client: 'pg',
+        connection: {
+            host: Config.dbHost,
+            user: Config.dbUser,
+            password: Config.dbUser,
+            database: Config.dbName,
+            port: 5432
+        },
+        migrations: {
+            directory: __dirname + '/db/pg/migrations'
+        },
+        seeds: {
+            directory: __dirname + '/db/pg/seeds/development'
+        }
+    },
+    production: {
+        client: 'pg',
+        connection: {
+            host: Config.dbHost,
+            user: Config.dbUser,
+            password: Config.dbUser,
+            database: Config.dbName,
+            ssl: true
+        },
+        migrations: {
+            directory: __dirname + '/db/pg/migrations'
+        },
+        seeds: {
+            directory: __dirname + '/db/pg/seeds/production'
+        }
+    }
+}
 
 export const Knex = knex(KnexConfiguration[Config.environment]);
