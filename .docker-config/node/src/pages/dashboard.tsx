c522c001b8082148cdad5083ad47c577e325e47e
Christopher D Jativa <cjativa@barnesfoundation.org>
2021-02-15 22:11:47 -0500

    padded page component usage

--- .docker-config/node/src/pages/dashboard.tsx
+++ .docker-config/node/src/pages/dashboard.tsx
@@ -1,7 +1,5 @@
-import { useState, useEffect } from "react";
-import Link from "next/link";
-
 import { Layout } from "../components/Layout";
+import { WrittenTutorialsDashboard } from "../components/dashboard/dashboard";
 import { TutorialDB } from "../../server/dataAccess/tutorials/entity";
 
 const Dashboard = ({ tutorials }) => {
@@ -9,71 +7,9 @@ const Dashboard = ({ tutorials }) => {
 
   return (
     <Layout pageTitle={title}>
-      <div className="dashboard padded-page">
-        <div className="padded-page__content">
-          <h1>Dashboard</h1>
-          <p>Here's where you'll find tutorials you've written</p>
-
-          {/** Tutorial list table */}
-          <table className="dashboard__table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Featured Image</th>
-                <th>Tags</th>
-                <th>Slug</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {tutorials.map((tutorial, index) => {
-                return (
-                  <tr
-                    className="dashboard__row"
-                    key={`${tutorial.title}__${index}`}
-                  >
-                    {/** Title and enabled */}
-                    <td>
-                      <span
-                        className={`circle ${
-                          tutorial.enabled ? "enabled" : "not-enabled"
-                        }`}
-                      />
-                      <Link href={`/write/${tutorial.id}`}>
-                        <a className="dashboard__row-link">{tutorial.title}</a>
-                      </Link>
-                    </td>
-
-                    {/** Featured Image */}
-                    <td>
-                      {tutorial.featuredImage ? (
-                        <img
-                          className="dashboard__row-fi"
-                          src={tutorial.featuredImage}
-                        />
-                      ) : (
-                        <i className="dashboard__row-fi fas fa-image " />
-                      )}
-                    </td>
-
-                    {/** Tags */}
-                    <td>
-                      {/* {tutorial.tags.map((tag) => {
-                                                <span>{tag}</span>
-                                            })} */}
-                    </td>
-
-                    {/** Slug Url */}
-                    <td>
-                      <p className="dashboard__row-slug">{tutorial.slug}</p>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <WrittenTutorialsDashboard
+        tutorials={tutorials}
+      />
     </Layout>
   );
 };
