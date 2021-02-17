f014ac79c2bbd0aba986147e03944523548acbcf
Christopher D Jativa <cjativa@barnesfoundation.org>
2021-02-16 23:15:20 -0500

    dotenv has 1st class support now

--- .docker-config/node/next.config.js
+++ .docker-config/node/next.config.js
@@ -1,11 +1,8 @@
 // next.config.js
 const withImages = require('next-images');
-const Dotenv = require("dotenv-webpack");
 
 module.exports = withImages({
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        // Add the new plugin to the existing webpack plugins
-        config.plugins.push(new Dotenv({ silent: true }));
         return config;
     },
     experimental: { optionalCatchAll: true }
