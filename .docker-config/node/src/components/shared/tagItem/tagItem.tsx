46b8e143b680295ea2f649ac7290a8e4ec00d77d
Christopher D Jativa <cjativa@barnesfoundation.org>
2021-02-20 17:27:56 -0500

    renamed component, added ability for own margin

--- .docker-config/node/src/components/shared/tagItem/tagItem.tsx
+++ .docker-config/node/src/components/shared/tagItem/tagItem.tsx
@@ -2,11 +2,21 @@
 interface ITagItemProps {
     tagId: number | string,
     tagName: string,
+    useMargin?: boolean,
 };
 
-export const TagItem = ({ tagId, tagName }: ITagItemProps) => {
+export const TagItem = ({ tagId, tagName, useMargin }: ITagItemProps) => {
+
+    let className = "tag-item";
+
+    // If the tag should provide its own margin 
+    // around itself
+    if (useMargin) {
+        className = className.concat(' tag-item--margin');
+    }
+
     return (
-        <div className="tag-item">
+        <div className={className}>
             {`#${tagName}`}
         </div>
     );
