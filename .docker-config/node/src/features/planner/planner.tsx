f35d1cc3304a65f86531e3056d18db5a1e896a46
Christopher D Jativa <cjativa@barnesfoundation.org>
2021-04-21 07:48:26 -0400

    separating completed and in progress posts

--- .docker-config/node/src/features/planner/planner.tsx
+++ .docker-config/node/src/features/planner/planner.tsx
@@ -2,6 +2,22 @@ import { TutorialCard } from '../../components/tutorialCard/tutorialCard';
 import { PaddedPage } from '../../components/paddedPage/paddedPage';
 
 export const Planner = ({ tutorials }) => {
+
+    const inProgressTutorials = [];
+    const completeTutorials = [];
+
+    // This sorting should take place server-side
+    // but for now, just do it here
+    tutorials.forEach((tutorial) => {
+        if (parseInt(tutorial.totalSectionCount) === parseInt(tutorial.completedSectionCount)) {
+            completeTutorials.push(tutorial);
+        }
+
+        else {
+            inProgressTutorials.push(tutorial);
+        }
+    });
+
     return (
         <PaddedPage
             heading="Planner"
@@ -12,22 +28,37 @@ export const Planner = ({ tutorials }) => {
                 {/** Tutorials in-progress */}
                 <div className="planner__section">
                     <h4>In-Progress</h4>
-                    <div className="planner__tutorials">
-                        {
-                            tutorials.map((tutorial, index) =>
-                                <TutorialCard
-                                    tutorial={tutorial}
-                                    key={`${tutorial.slug}__${index}`}
-                                />
-                            )
-                        }
-                    </div>
+                    {
+                        (inProgressTutorials.length > 0)
+                            ?
+                            <div className="planner__tutorials">
+                                {inProgressTutorials.map((tutorial, index) =>
+                                    <TutorialCard
+                                        tutorial={tutorial}
+                                        key={`${tutorial.slug}__${index}`}
+                                    />
+                                )}
+                            </div>
+                            : <p>You don't have any tutorials in progress yet!</p>
+                    }
                 </div>
 
                 {/** Tutorials that have been completed */}
                 <div className="planner__section">
                     <h4>Complete</h4>
-                    <p>You haven't completed a tutorial yet!</p>
+                    {
+                        (completeTutorials.length > 0)
+                            ?
+                            <div className="planner__tutorials">
+                                {completeTutorials.map((tutorial, index) =>
+                                    <TutorialCard
+                                        tutorial={tutorial}
+                                        key={`${tutorial.slug}__${index}`}
+                                    />
+                                )}
+                            </div>
+                            : <p>You haven't completed a tutorial yet!</p>
+                    }
                 </div>
 
             </div>
