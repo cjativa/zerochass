6ed09a90beb7c74d1b48e567d0490713e0437351
Christopher D Jativa <cjativa@barnesfoundation.org>
2021-04-22 00:07:23 -0400

    logic for syncing tags

--- .docker-config/node/server/dataAccess/tag/index.ts
+++ .docker-config/node/server/dataAccess/tag/index.ts
@@ -18,15 +18,18 @@ export class TagDAO {
     };
 
     public static async relateWithTutorial(tags: string[], tutorialId: string | number) {
-        const insertableTags = tags.map((tag) => {
-            return { tag }
-        });
 
         // Add any tags that don't exist in the schema
+        const insertableTags = tags.map((tag) => {
+            return { tag };
+        });
         await Knex('tags')
             .insert(insertableTags)
             .onConflict('tag')
             .ignore();
+
+        // Remove any tags that should no longer be associated with the tutorial
+        await TagDAO.validateWithTutorial(tags, tutorialId);
 
         // Get the ids for these tags
         const tagIds = await Knex('tags')
@@ -45,5 +48,37 @@ export class TagDAO {
             .insert(tutorialTagPairs)
             .onConflict(['tagId', 'tutorialId'])
             .ignore();
+    };
+
+    private static async validateWithTutorial(tags: string[], tutorialId: string | number) {
+
+        // Find out which tags are associated with this tutorial right now
+        const tagsInDatabase = (await Knex.raw(`
+        SELECT tags.tag, tags.id
+        FROM tags
+        INNER JOIN tutorial_tag_relations ttr
+        ON 
+            ttr."tagId" = tags.id
+        WHERE 
+            ttr."tutorialId" = ${tutorialId};
+        `)).rows;
+
+        // Any not provided, or any provided that aren't what the database has on record
+        // means we need to remove from the database
+        const tagIdsToRemove = tagsInDatabase.reduce((acc, tagItem) => {
+            const tagShouldBeRemoved = !tags.find((tag) => tag.toLowerCase() === tagItem.tag.toLowerCase());
+            if (tagShouldBeRemoved && tagItem.hasOwnProperty('id')) {
+                acc.push(tagItem.id);
+            }
+
+            return acc;
+        }, [] as number[]);
+
+        if (tagIdsToRemove.length > 0) {
+            await Knex('tutorial_tag_relations')
+                .delete()
+                .whereIn('tagId', tagIdsToRemove)
+                .andWhere({ tutorialId })
+        }
     };
 };
